e6ff74ead0896861741457a251eebb7f3ac70270
Shadman Ahmed <shadifmg@gmail.com>
2024-11-10 13:42:40 -0600

    feat: fixed functionality of links on sidebar and hover function, fixed spacing of copyright. make toggle button fill when toggled

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -2,7 +2,16 @@
 
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsSidebarCollapsed } from "@/app/state";
-import { Layout, LucideIcon, Menu } from "lucide-react";
+import {
+  Archive,
+  CircleDollarSign,
+  Clipboard,
+  Layout,
+  LucideIcon,
+  Menu,
+  SlidersHorizontal,
+  User,
+} from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -30,7 +39,7 @@ const SidebarLink = ({
         className={`cursor-pointer flex items-center ${
           isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"
         }
-          hover:text-blue-500 hover: bg-blue-100 gap-3 transition-colors ${
+          hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${
             isActive ? "bg-blue-200 text-white" : ""
           }
         `}
@@ -50,38 +59,38 @@ const SidebarLink = ({
 
 const Sidebar = () => {
   const dispatch = useAppDispatch();
-  const isSideBarCollapsed = useAppSelector(
+  const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed
   );
 
   const toggleSidebar = () => {
-    dispatch(setIsSidebarCollapsed(!isSideBarCollapsed));
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
   };
 
   const sidebarClassNames = `fixed flex flex-col ${
-    isSideBarCollapsed ? "w-0 md:w-16" : "w-72 md:64"
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
   } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
 
   return (
     <div className={sidebarClassNames}>
       {/* TOP LOGO */}
-
       <div
         className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
-          isSideBarCollapsed ? "px-5" : "px-8"
+          isSidebarCollapsed ? "px-5" : "px-8"
         }`}
       >
         <div>logo</div>
         <h1
           className={`${
-            isSideBarCollapsed ? "hidden" : "block"
+            isSidebarCollapsed ? "hidden" : "block"
           } font-extrabold text-2xl`}
         >
           SHADSTOCK
         </h1>
-
         <button
-          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          className={`md:hidden px-3 py-3 rounded-full ${
+            isSidebarCollapsed ? "bg-gray-100" : "bg-gray-200"
+          } hover:bg-blue-100`}
           onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
@@ -94,12 +103,42 @@ const Sidebar = () => {
           href="/dashboard"
           icon={Layout}
           label="Dashboard"
-          isCollapsed={isSideBarCollapsed}
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/inventory"
+          icon={Archive}
+          label="Inventory"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/products"
+          icon={Clipboard}
+          label="Products"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/users"
+          icon={User}
+          label="Users"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/settings"
+          icon={SlidersHorizontal}
+          label="Settings"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/expenses"
+          icon={CircleDollarSign}
+          label="Expenses"
+          isCollapsed={isSidebarCollapsed}
         />
       </div>
 
       {/* FOOTER */}
-      <div>
+      <div className={`${isSidebarCollapsed} ? "hidden" : "block"} mb-10`}>
         <p className="text-center text-xs text-gray-500">
           &copy; 2024 Shadstock
         </p>
