a344abca2a5b91de139614b78ab9c4f9a5cccc43
Shadman Ahmed <shadifmg@gmail.com>
2024-11-10 13:11:22 -0600

    feat: added dashboard link to sidebar nav

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -2,8 +2,51 @@
 
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsSidebarCollapsed } from "@/app/state";
-import { Menu } from "lucide-react";
+import { Layout, LucideIcon, Menu } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
+
+interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  isCollapsed,
+}: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+
+  return (
+    <Link href={href}>
+      <div
+        className={`cursor-pointer flex items-center ${
+          isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"
+        }
+          hover:text-blue-500 hover: bg-blue-100 gap-3 transition-colors ${
+            isActive ? "bg-blue-200 text-white" : ""
+          }
+        `}
+      >
+        <Icon className="w-6 h-6 text-gray-700" />
+        <span
+          className={`${
+            isCollapsed ? "hidden" : "block"
+          } font-medium text-gray-700`}
+        >
+          {label}
+        </span>
+      </div>
+    </Link>
+  );
+};
 
 const Sidebar = () => {
   const dispatch = useAppDispatch();
@@ -45,8 +88,15 @@ const Sidebar = () => {
         </button>
       </div>
 
-      {/* TOP LOGO */}
-      <div className="flex-grow mt-8">{/* Links Here */}</div>
+      {/* LINKS */}
+      <div className="flex-grow mt-8">
+        <SidebarLink
+          href="/dashboard"
+          icon={Layout}
+          label="Dashboard"
+          isCollapsed={isSideBarCollapsed}
+        />
+      </div>
 
       {/* FOOTER */}
       <div>
